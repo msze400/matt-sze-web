242771f5f19d883f0ad5d1b6e0c0c5380287549f
msze400 <msze400@gmail.com>
2022-01-26 01:13:17 -0500

    blogs showing

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { db } from '../firebase-config'
 import { collection, getDocs } from 'firebase/firestore'
+import userEvent from '@testing-library/user-event'
 
 export default function Blogs() {
     const [blogs, setBlogs] = useState([])
@@ -9,11 +10,30 @@ export default function Blogs() {
     useEffect(() => {
         const getBlogs = async () => {
             const data = await getDocs(blogsCollectionRef)
+            setBlogs(
+                data.docs.map((doc) => ({
+                    ...doc.data(),
+                    id: doc.id,
+                }))
+            )
             console.log(data)
+
+            // console.log('BLOGS', blogs)
         }
 
         getBlogs()
     }, [])
 
-    return <h1>hello</h1>
+    return (
+        <div>
+            {blogs.map((blog) => {
+                return (
+                    <div>
+                        <p>Topic: {blog.topic}</p>
+                        <p>Summary: {blog.summary}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
